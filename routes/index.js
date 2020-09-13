3b4130f094ee80b91b3a89e85edcd255bf2f4060
Manuel M Arizmendi <23641126+marizmendi@users.noreply.github.com>
2020-09-13 14:22:54 -0500

    basic upload

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,20 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/upload', function(req, res, next) {
-  console.log(req.files);
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return res.status(400).send('No files were uploaded.');
+  }
+
+  // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+  let image = req.files.image;
+
+  // Use the mv() method to place the file somewhere on your server
+  image.mv('./image.jpg', function(err) {
+    if (err)
+      return res.status(500).send(err);
+
+    res.send('File uploaded!');
+  });
 });
 
 module.exports = router;
