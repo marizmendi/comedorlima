568056c31cae555471f3fa4774157de34e281702
Manuel M Arizmendi <23641126+marizmendi@users.noreply.github.com>
2020-09-13 13:58:00 -0500

    remove users and add upload route

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Comedor Lima' });
 });
 
+router.post('/upload', function(req, res) {
+  console.log(req.files.foo); // the uploaded file object
+});
+
 module.exports = router;
